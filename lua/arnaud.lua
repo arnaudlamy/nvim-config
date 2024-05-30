b04d0503669a37e55194f28b9cb1022ddac3f38a
Arnaud Lamy <arnaud@powerz.tech>
2024-05-30 18:20:06 +0200

    Json conf

--- lua/arnaud.lua
+++ lua/arnaud.lua
@@ -1,3 +1,6 @@
 -- Remapping Ctrl + p to :Leaderf file
 vim.api.nvim_set_keymap('n', '<C-p>', ':Leaderf file<CR>', { noremap = true, silent = true })
 
+-- FormatJson
+vim.cmd('command! FormatJson %!python -m json.tool')
+
